9bffb4a5982351c1b1156da9944e76363ab848ea
Alessandro Autiero <alautiero@gmail.com>
2025-03-06 21:14:02 +0100

    TLS

--- src/main/java/it/auties/leap/http/HttpVersion.java
+++ src/main/java/it/auties/leap/http/HttpVersion.java
@@ -1,6 +1,7 @@
 package it.auties.leap.http;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public enum HttpVersion {
     HTTP_1_1("HTTP/1.1".getBytes(StandardCharsets.US_ASCII)),
@@ -10,6 +11,18 @@ public enum HttpVersion {
     private final byte[] encodedName;
     HttpVersion(byte[] encodedName) {
         this.encodedName = encodedName;
+    }
+
+    public static Optional<HttpVersion> of(int major, int minor) {
+        if(major == 1 && minor == 1) {
+            return Optional.of(HTTP_1_1);
+        }else if(major == 2 && minor == 0) {
+            return Optional.of(HTTP_2);
+        }else if(major == 3 && minor == 0) {
+            return Optional.of(HTTP_3);
+        }else {
+            return Optional.empty();
+        }
     }
 
     public byte[] encodedName() {
--- src/main/java/it/auties/leap/socket/SocketIO.java
+++ src/main/java/it/auties/leap/socket/SocketIO.java
@@ -4,4 +4,5 @@ import it.auties.leap.socket.async.AsyncSocketIO;
 import it.auties.leap.socket.blocking.BlockingSocketIO;
 
 public sealed interface SocketIO permits AsyncSocketIO, BlockingSocketIO {
+    <V> V getOption(SocketOption<V> option);
 }
